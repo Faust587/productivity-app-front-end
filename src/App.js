a8600a454a5340abfe2eb19eac2142fe5448f5b8
Yura Synkulych <kri3g0vec@gmail.com>
2022-06-10 23:06:13 +0200

    Created routes (Home route and Not defined route)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import './App.css';
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App"/>
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={"Home Page"} />
+              <Route path="*" element={"ERROR"} />
+          </Routes>
+      </BrowserRouter>
   );
 }
 
